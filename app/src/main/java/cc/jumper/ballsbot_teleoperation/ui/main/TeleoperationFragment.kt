3c1e9968ae2e26439f15b027de0feb66acdb2c22
Oleg Nurtdinov <j@jumper.cc>
2022-05-02 01:14:13 +0300

    layout

--- app/src/main/java/cc/jumper/ballsbot_teleoperation/ui/main/TeleoperationFragment.kt
+++ app/src/main/java/cc/jumper/ballsbot_teleoperation/ui/main/TeleoperationFragment.kt
@@ -1,62 +1,29 @@
 package cc.jumper.ballsbot_teleoperation.ui.main
 
+import android.R
+import android.content.res.Configuration
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
-import cc.jumper.ballsbot_teleoperation.R
 import cc.jumper.ballsbot_teleoperation.TeleoperationApplication
 import cc.jumper.ballsbot_teleoperation.data.Connection
-import cc.jumper.ballsbot_teleoperation.databinding.EditConnectionFragmentBinding
 import cc.jumper.ballsbot_teleoperation.databinding.FragmentTeleoperationBinding
 import cc.jumper.ballsbot_teleoperation.models.ConnectionsViewModel
 import cc.jumper.ballsbot_teleoperation.models.ConnectionsViewModelFactory
 import cc.jumper.ballsbot_teleoperation.models.TeleoperationViewModel
 import cc.jumper.ballsbot_teleoperation.models.TeleoperationViewModelFactory
 
+
 class TeleoperationFragment : Fragment() {
-    private val hideHandler = Handler()
-
-    @Suppress("InlinedApi")
-    private val hidePart2Runnable = Runnable {
-        val flags =
-            View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        activity?.window?.decorView?.systemUiVisibility = flags
-        (activity as? AppCompatActivity)?.supportActionBar?.hide()
-    }
-    private var visible: Boolean = false
-    private val hideRunnable = Runnable { hide() }
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private val delayHideTouchListener = View.OnTouchListener { _, _ ->
-        if (AUTO_HIDE) {
-            delayedHide(AUTO_HIDE_DELAY_MILLIS)
-        }
-        false
-    }
-
-    private var fullscreenContent: View? = null
-
     private var _binding: FragmentTeleoperationBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     private val navigationArgs: EditConnectionFragmentArgs by navArgs()
@@ -77,8 +44,7 @@ class TeleoperationFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentTeleoperationBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -87,15 +53,12 @@ class TeleoperationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        visible = true
-
-        // Set up the user interaction to manually show or hide the system UI.
-        fullscreenContent?.setOnClickListener { toggle() }
-
         val connectionId = navigationArgs.connectionId
         viewModelConnection.getConnection(connectionId)
             .observe(this.viewLifecycleOwner) { selectedConnection ->
                 connection = selectedConnection
+                (activity as AppCompatActivity?)?.supportActionBar?.title =
+                    selectedConnection.description
                 viewModelTeleoperation.connectionInfo = selectedConnection
                 if (!viewModelTeleoperation.updatesRunning()) {
                     viewModelTeleoperation.startUpdates()
@@ -114,88 +77,51 @@ class TeleoperationFragment : Fragment() {
         binding.viewModelConnection = viewModelConnection
         binding.viewModelTeleoperation = viewModelTeleoperation
         binding.thisFragment = this
+
+        updateLayout(resources.configuration)
+    }
+
+    private fun updateLayout(newConfig: Configuration) {
+        // TODO
+//        val paramsFront = binding.frontCamera.layoutParams as ConstraintLayout.LayoutParams
+//        val paramsRear = binding.rearCamera.layoutParams as ConstraintLayout.LayoutParams
+//
+//        paramsFront.startToStart = binding.parentLayout.id
+//        paramsFront.topToTop = binding.parentLayout.id
+//
+//        paramsRear.endToEnd = binding.parentLayout.id
+//        paramsRear.bottomToBottom = binding.parentLayout.id
+//
+//        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+//            paramsFront.bottomToBottom = binding.parentLayout.id
+//            paramsFront.endToStart = binding.rearCamera.id
+//
+//            paramsRear.topToTop = binding.parentLayout.id
+//            paramsRear.startToEnd = binding.frontCamera.id
+//        } else {
+//            paramsFront.bottomToTop = binding.rearCamera.id
+//            paramsFront.endToEnd = binding.parentLayout.id
+//
+//            paramsRear.topToBottom = binding.frontCamera.id
+//            paramsRear.startToStart = binding.parentLayout.id
+//        }
+//
+//        binding.parentLayout.invalidate()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        updateLayout(newConfig)
     }
 
     override fun onResume() {
         super.onResume()
         activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100)
     }
 
     override fun onPause() {
         super.onPause()
         activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-
-        // Clear the systemUiVisibility flag
-        activity?.window?.decorView?.systemUiVisibility = 0
-        show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        fullscreenContent = null
-    }
-
-    private fun toggle() {
-        if (visible) {
-            hide()
-        } else {
-            show()
-        }
-    }
-
-    private fun hide() {
-        // Hide UI first
-        visible = false
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
-    }
-
-    @Suppress("InlinedApi")
-    private fun show() {
-        // Show the system bar
-        fullscreenContent?.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        visible = true
-
-        // Schedule a runnable to display UI elements after a delay
-        hideHandler.removeCallbacks(hidePart2Runnable)
-        (activity as? AppCompatActivity)?.supportActionBar?.show()
-    }
-
-    /**
-     * Schedules a call to hide() in [delayMillis], canceling any
-     * previously scheduled calls.
-     */
-    private fun delayedHide(delayMillis: Int) {
-        hideHandler.removeCallbacks(hideRunnable)
-        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
-    }
-
-    companion object {
-        /**
-         * Whether or not the system UI should be auto-hidden after
-         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
-         */
-        private const val AUTO_HIDE = true
-
-        /**
-         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
-         * user interaction before hiding the system UI.
-         */
-        private const val AUTO_HIDE_DELAY_MILLIS = 3000
-
-        /**
-         * Some older devices needs a small delay between UI widget updates
-         * and a change of the status and navigation bar.
-         */
-        private const val UI_ANIMATION_DELAY = 300
     }
 
     override fun onDestroyView() {
